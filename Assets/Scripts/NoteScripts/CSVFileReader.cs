478e456728c2ce21ccf4987e0edadc82456d28b0
ChoGyeonran <soul7812@naver.com>
2016-12-29 18:29:08 +0900

    장애물 CSV 파일 작업 시작_CSV 파일 파싱

---
+++ Assets/Scripts/NoteScripts/CSVFileReader.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+using UnityEngine.UI;
+
+using System.Text;
+
+public class CSVFileReader : MonoBehaviour {
+
+    void Awake()
+    {
+        LoadCSVFile("Assets\\CSV\\test.csv");
+    }
+
+    void Update()
+    {
+    }
+
+    void LoadCSVFile(string fileName)
+    {
+
+
+        StreamReader sr = new StreamReader(fileName, System.Text.Encoding.Default);
+
+        string strLineValue = null;
+        string[] keys = null;
+        string[] values = null;
+
+        while ((strLineValue = sr.ReadLine()) != null)
+        {
+            // Must not be empty.
+            if (string.IsNullOrEmpty(strLineValue)) return;
+            values = strLineValue.Split(',');
+        }
+    }
+}
+
