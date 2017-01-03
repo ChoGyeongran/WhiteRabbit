7da6a84572c48bbfdfaea147fcbfd155b0635b04
ChoGyeongran <soul7812@naver.com>
2017-01-03 10:41:12 +0900

    빌드 해상도 수정

---
+++ Assets/Scripts/SettingScripts/SettingButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingButton : MonoBehaviour {
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
