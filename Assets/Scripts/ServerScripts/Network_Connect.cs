80096fe39b481e5d93ed305d23cf5ea9ff1f0c3d
ChoGyeongran <soul7812@naver.com>
2017-01-05 18:30:51 +0900

    Photon Cloud 에셋 추가

---
+++ Assets/Scripts/ServerScripts/Network_Connect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Network_Connect : Photon.MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings(null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
