1479e65710549f9cba1777de20d200f04bfbe1f5
ChoGyeongran <soul7812@naver.com>
2017-01-09 18:32:44 +0900

    mysql dll 참조 파일 추가

--- Assets/Scripts/DatabaseScripts/DatabaseConnect.cs
+++ Assets/Scripts/DatabaseScripts/DatabaseConnect.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Data;
+using System.ComponentModel;
+using System.Linq;
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mysql.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
-public class DatabaseConnect : MonoBehaviour {
+public class DatabaseConnect {
 
 	// Use this for initialization
 	void Start () {
-        string constr = "Server=192.168.0.200";
         
-        		
-	}
+        MySqlConnection Con = new MySqlConnection();
+        Con.ConnectionString = "Data Source=localhost;Database=TEST;"
+                                                              + "User Id=root;Password=tlfcjs42" + ";charset=euckr";
+        Con.Open();
+        string Rec;
+        MySqlCommand Com = new MySqlCommand("SELECT * FROM scoretable", Con);
+        MySqlDataReader R;
+        R = Com.ExecuteReader();
+
+
+        while (R.Read())
+        {
+            Rec = string.Format("ID: {0}, SCORE: {1}", R["id"], R["score"]);
+        }
+
+        Debug.Log("로딩 성공");
+
+        R.Close();
+        Con.Close();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
